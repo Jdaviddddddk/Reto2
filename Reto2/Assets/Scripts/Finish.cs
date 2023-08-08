795d14e07fb3cd64145b6c7e417b35a0f8997717
Jdaviddddddk <jhonnatana06@gmail.com>
2023-08-07 22:33:11 -0500

    Terminando detalles

--- Reto2/Assets/Scripts/Finish.cs
+++ Reto2/Assets/Scripts/Finish.cs
@@ -5,24 +5,27 @@ using UnityEngine.SceneManagement;
 
 public class Finish : MonoBehaviour
 {
-    private void OnTriggerEnter(Collider Collision)
-    {
-        // Verificar si el objeto que tocó la bandera tiene el tag "Player"
-        if (Collision.CompareTag("Player"))
-        {
-            // Obtener el índice del siguiente nivel
-            int nextLevelIndex = SceneManager.GetActiveScene().buildIndex + 1;
+    private AudioSource finishSound;
 
-            // Cargar el siguiente nivel
-            if (nextLevelIndex < SceneManager.sceneCountInBuildSettings)
-            {
-                SceneManager.LoadScene(nextLevelIndex);
-            }
-            else
-            {
-                // Si ya no hay más niveles, puedes mostrar un mensaje o hacer otra acción
-                Debug.Log("¡Has completado todos los niveles!");
-            }
+    private bool levelCompleted = false;
+
+    private void Start()
+    {
+        finishSound = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Player" && !levelCompleted)
+        {
+            finishSound.Play();
+            levelCompleted = true;
+            Invoke("CompleteLevel", 1f);
         }
+    }
+
+    private void CompleteLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
